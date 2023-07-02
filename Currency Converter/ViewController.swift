dc8d5634318decb893b746104c79f16e8b447761
Owais Quadri <owaisquadri01@gmail.com>
2023-07-02 05:39:02 -0400

    Create Simple converter between USD and CAD

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var endCurrency: UISegmentedControl!
+    @IBOutlet weak var conversionRateInput: UITextField!
+    @IBOutlet weak var toBeConvertedInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func convertButtonTouchUpInside(_ sender: Any) {
+//        get values from text fields
+        let rate = Double(conversionRateInput.text!) ?? 0
+        let amountToBeConverted = Double(toBeConvertedInput.text!) ?? 0
+        let result = rate * amountToBeConverted
+        resultLabel.text = result.formatted(.currency(code: "USD"))+" \(endCurrency.selectedSegmentIndex == 0 ? "CAD" : "USD")"
+    }
+    
 
 }
 
